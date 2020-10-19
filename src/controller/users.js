7d1c866e835b8d0c25c3abcd8128e68b18d292a4
Glitchfer <arif.0496.rahman@gmail.com>
2020-10-19 14:11:49 +0700

    up

--- src/controller/users.js
+++ src/controller/users.js
@@ -65,9 +65,6 @@ module.exports = {
             email: checkDataUsers[0].user_email,
             phone: checkDataUsers[0].user_phone,
             login: new Date(),
-            logout: new Date("0000-00-00 00:00:00"),
-            token: "00:00:00",
-            sisa_token: "00:00:00",
           };
           const result = await postLogin(loginInfo);
           const setData2 = {
